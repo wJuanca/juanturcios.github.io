9abec9d8198e19b916c1a7bb53ba70bebf0fcbfa
wJuanca <juancarlosturcios2001@gmail.com>
2025-01-20 00:33:14 -0600

    deploy de page

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://wjuanca.github.io/juanturcios.github.io/',
+  base: 'juanturcios.github.io',
   integrations: [tailwind()],
 });
