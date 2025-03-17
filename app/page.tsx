6d9f248b39089666761cd679876f6edbe3996709
wJuanca <juancarlosturcios2001@gmail.com>
2025-03-16 22:48:50 -0600

    Agregue la dependecia de Ana;ytics de vercel

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Skills from "@/components/skills"
 import Tools from "@/components/tools"
 import Footer from "@/components/footer"
 import Contact from "@/components/contact"
+import { Analytics } from "@vercel/analytics/react"
 
 
 export default function Home() {
@@ -15,6 +16,7 @@ export default function Home() {
       <Tools />
       <Contact />
       <Footer />
+      <Analytics />
     </main>
   )
 }
